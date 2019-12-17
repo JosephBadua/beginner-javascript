7951fa40c5b3bbf79b71dd956b9de483005ca915
JosephBadua <josephbadua1006@gmail.com>
2019-12-17 15:18:40 -0800

    tab panels

--- exercises/35 - Scroll To Accept/scroll-to-accept.js
+++ exercises/35 - Scroll To Accept/scroll-to-accept.js
@@ -1,1 +1,19 @@
 console.log('IT WORKS!');
+
+const accept = document.querySelector(`.terms-and-conditions`);
+const watch = document.querySelector(`.watch`);
+const button = document.querySelector(`.accept`);
+function observeCallback(payload) {
+  if (payload[0].intersectionRatio === 1) {
+    button.disabled = false;
+    observe.unobserve(accept.lastElementChild);
+  } else {
+    button.disabled = true;
+  }
+}
+const observe = new IntersectionObserver(observeCallback, {
+  root: accept,
+  threshold: 1,
+});
+
+observe.observe(accept.lastElementChild);
--- exercises/36 - Tabs/tabs.js
+++ exercises/36 - Tabs/tabs.js
@@ -1,1 +1,16 @@
-console.log('ya ya wes we get it.. IT WORKS!');
+const tabs = document.querySelector(`.tabs`);
+const buttons = tabs.querySelectorAll(`[role="tab"]`);
+const tabPanel = tabs.querySelectorAll(`[role="tabpanel"]`);
+
+function handleTabClick(event) {
+  console.log(event.currentTarget);
+  tabPanel.forEach(panel => (panel.hidden = true));
+  buttons.forEach(tab => tab.setAttribute(`aria-selected`, false));
+  event.currentTarget.setAttribute(`aria-selected`, true);
+  const buttonId = event.currentTarget.id;
+  const thisThing = tabs.querySelector(`[aria-labelledby="${buttonId}"]`);
+  thisThing.hidden = false;
+  console.log(buttonId);
+}
+
+buttons.forEach(button => button.addEventListener(`click`, handleTabClick));
