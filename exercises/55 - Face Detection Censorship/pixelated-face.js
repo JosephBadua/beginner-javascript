28f7579ffdea5d1402593213772c0f54887b7cb5
JosephBadua <josephbadua1006@gmail.com>
2019-12-23 02:59:49 -0800

    face track and censorship

--- exercises/55 - Face Detection Censorship/pixelated-face.js
+++ exercises/55 - Face Detection Censorship/pixelated-face.js
@@ -1,42 +1,37 @@
-const video = document.querySelector('.webcam');
-const canvas = document.querySelector('.video');
-const ctx = canvas.getContext('2d');
-const faceCanvas = document.querySelector('.face');
-const faceCtx = faceCanvas.getContext('2d');
-const faceDetector = new window.FaceDetector();
-const optionsInputs = document.querySelectorAll(
-  '.controls input[type="range"]'
-);
-
+const webcam = document.querySelector(`.webcam`);
+const video = document.querySelector(`.video`);
+const canvas = document.querySelector(`.face`);
+const context = canvas.getContext(`2d`);
+const FaceContext = canvas.getContext(`2d`);
+const optionInput = document.querySelectorAll(`.controls input[type="range"]`);
 const options = {
-  SIZE: 10,
-  SCALE: 1.35,
+  size: 10,
+  scale: 1.5,
 };
 
 function handleOption(event) {
   const { value, name } = event.currentTarget;
   options[name] = parseFloat(value);
 }
-optionsInputs.forEach(input => input.addEventListener('input', handleOption));
 
-// Write a fucntion that will populate the users video
+optionInput.forEach(input => input.addEventListener(`input`, handleOption));
+const faceDetector = new window.FaceDetector();
+
 async function populateVideo() {
   const stream = await navigator.mediaDevices.getUserMedia({
     video: { width: 1280, height: 720 },
   });
-  video.srcObject = stream;
-  await video.play();
-  // size the canvases to be the same size as the video
-  console.log(video.videoWidth, video.videoHeight);
-  canvas.width = video.videoWidth;
-  canvas.height = video.videoHeight;
-  faceCanvas.width = video.videoWidth;
-  faceCanvas.height = video.videoHeight;
+  webcam.srcObject = stream;
+  await webcam.play();
+  console.log(webcam.videoWidth, webcam.videoHeight);
+  canvas.width = webcam.videoWidth;
+  canvas.height = webcam.videoHeight;
+  video.width = webcam.videoWidth;
+  video.height = webcam.videoHeight;
 }
 
 async function detect() {
-  const faces = await faceDetector.detect(video);
-  // ask the browser when the next animation frame is, and tell it to run detect for us
+  const faces = await faceDetector.detect(webcam);
   faces.forEach(drawFace);
   faces.forEach(censor);
   requestAnimationFrame(detect);
@@ -44,40 +39,33 @@ async function detect() {
 
 function drawFace(face) {
   const { width, height, top, left } = face.boundingBox;
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-  ctx.strokeStyle = '#ffc600';
-  ctx.lineWidth = 2;
-  ctx.strokeRect(left, top, width, height);
+  context.clearRect(0, 0, canvas.width, canvas.height);
+  context.lineWidth = 2;
+  context.strokeRect(left, top, width, height);
 }
 
 function censor({ boundingBox: face }) {
-  faceCtx.imageSmoothingEnabled = false;
-  faceCtx.clearRect(0, 0, faceCanvas.width, faceCanvas.height);
-  // draw the small face
-  faceCtx.drawImage(
-    // 5 source args
-    video, // where does the source come from?
-    face.x, // where do we start the source pull from?
+  FaceContext.drawImage(
+    webcam,
+    face.x,
     face.y,
     face.width,
     face.height,
-    // 4 draw args
-    face.x, // where should we start drawing the x and y?
+    face.x,
     face.y,
-    options.SIZE,
-    options.SIZE
+    options.size,
+    options.size
   );
-  // draw the small face back on, but scale up
 
-  const width = face.width * options.SCALE;
-  const height = face.height * options.SCALE;
-  faceCtx.drawImage(
-    faceCanvas, // source
-    face.x, // where do we start the source pull from?
+  const width = face.width * options.scale;
+  const height = face.height * options.scale;
+
+  FaceContext.drawImage(
+    canvas,
+    face.x,
     face.y,
-    options.SIZE,
-    options.SIZE,
-    // Drawing args
+    options.size,
+    options.size,
     face.x - (width - face.width) / 2,
     face.y - (height - face.height) / 2,
     width,
