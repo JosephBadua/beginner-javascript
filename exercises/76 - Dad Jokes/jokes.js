f476ea9c388177bdcad7eee8d01a0813695218ef
Joseph Badua <52088997+JosephBadua@users.noreply.github.com>
2020-01-08 16:21:09 -0800

    dad joke and api recpies

--- exercises/76 - Dad Jokes/jokes.js
+++ exercises/76 - Dad Jokes/jokes.js
@@ -8,3 +8,36 @@ const buttonText = [
   'please stop',
   'that was the worst one',
 ];
+
+const jokeButton = document.querySelector(`.getJoke`);
+const jokeHolder = document.querySelector(`.joke p`);
+
+async function fetchJoke(){
+  const response = await fetch(`https://icanhazdadjoke.com`, {
+    headers: {
+      Accept: 'application/json',
+    }
+  })
+  const joke = await response.json();
+  return joke
+}
+
+function randomItemFromArray(arr, not){
+  const item = arr[Math.floor(Math.random() * arr.length)]
+  if (item === not) {
+    return randomItemFromArray(arr, not)
+  }
+  return item;
+}
+
+async function handleClick(){
+  const { joke } = await fetchJoke()
+  console.log(joke);
+  jokeHolder.textContent = joke;
+  jokeButton.textContent = randomItemFromArray(buttonText, jokeButton.textContent);
+}
+
+jokeButton.addEventListener(`click`, handleClick)
+
+fetchJoke();
+
